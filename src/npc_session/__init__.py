c56ffd713e1aff673a6f3dc7de6dc6bb0ee60a64
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2023-08-12 22:12:13 +0000

    Make parsing available

--- src/npc_session/__init__.py
+++ src/npc_session/__init__.py
@@ -1,7 +1,9 @@
 from npc_session.records import (
     DateRecord,
     DatetimeRecord,
+    TimeRecord,
     ProjectRecord,
     SessionRecord,
     SubjectRecord,
 )
+from npc_session.parsing import *
