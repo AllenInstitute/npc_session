f405836b23cc3f7218231e422c7d7bffca312ed5
bjhardcastle <ben.hardcastle@alleninstitute.org>
2023-08-17 19:27:14 -0700

    Fix not exported attrs

--- src/npc_session/__init__.py
+++ src/npc_session/__init__.py
@@ -1,9 +1,2 @@
 from npc_session.parsing import *
-from npc_session.records import (
-    DateRecord,
-    DatetimeRecord,
-    ProjectRecord,
-    SessionRecord,
-    SubjectRecord,
-    TimeRecord,
-)
+from npc_session.records import *
